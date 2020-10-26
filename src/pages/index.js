a23cf4b501d32a656008981ba501f2a08a264f43
Krishnakanth Yachareni <yacharenikrishnakanth@gmail.com>
2020-10-26 20:39:48 +0530

    added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { About, Contact, Hero, Jobs, Layout, Projects } from '@components';
 import PropTypes from 'prop-types';
+import React from 'react';
 import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -13,7 +13,6 @@ const IndexPage = ({ location }) => (
       <Hero />
       <About />
       <Jobs />
-      <Featured />
       <Projects />
       <Contact />
     </StyledMainContainer>
